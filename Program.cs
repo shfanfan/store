7ed1892235045f4cc2e4c3c258c9ae258a53b5dd
shfanfan <shfanfan@yahoo.com>
2022-02-23 15:13:58 +0200

    added both employee class and main store array

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[4];
+            Console.WriteLine("total salaries = " + GetTotalSalaries(employees).ToString());
+        }
+
+        static int GetTotalSalaries(Employee[] employees)
+        {
+            return -1;
+        }
+    }
+}
---
+++ employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace store
+{
+    class Employee
+    {
+        string name;
+        int salary;
+        int yearsSeniority;
+
+        private Employee(){}
+
+        public Employee(String name, int salary, int yearsSeniority)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.yearsSeniority = yearsSeniority;
+        }
+
+        public int GetSalary()
+        {
+            return salary;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetYearsSeniority()
+        {
+            return yearsSeniority;
+        }
+    }
+}
