d3272bdfbd57fb43170c2ea749882ff5b394ceda
shfanfan <shfanfan@yahoo.com>
2022-02-24 08:46:19 +0200

    naming conventions fix

---
+++ name.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace store
+{
+    class Name
+    {
+        private string firstName;
+        private string lastName;
+        private Name() { }
+        public Name (string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public void printName() {
+            Console.WriteLine("my name is " + firstName + " " + lastName);
+        }
+        public int howLongIsName()
+        {
+            return firstName.Length + lastName.Length;
+        }
+    }
+
+}
